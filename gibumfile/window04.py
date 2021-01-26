f394684d14d1d3c1111af5fe6b819562e1e15f81
bbasung1 <bbasung1@daum.net>
2021-01-26 17:43:43 +0900

    remove test

--- gibumfile/window04.py
+++ gibumfile/window04.py
@@ -1,17 +1,3 @@
-import os
-import hashlib
-import numpy
-test = os.listdir("./")
-k = []
-for a in test:
-    if(os.path.isdir(a)):
-        test.remove(a)
-        continue
-    f = open(a, "rb")
-    data = f.read()
-    k.append(hashlib.md5(data).hexdigest())
-print(test)
-print(k)
-for b in k:
-    alpha = numpy.array(k)
-    print(numpy.where(alpha == b))
+a = [1,2,3,1]
+a.remove(1)
+print(a)
